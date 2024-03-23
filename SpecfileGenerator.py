327fa434c342b7fa47808c9de2efd33dbe04a6cd
August-Brandt <aubr@itu.dk>
2024-03-23 16:26:14 +0100

    initial commit

---
+++ SpecfileGenerator.py
@@ -0,0 +1,24 @@
+import random
+import string
+
+lines = 1000
+header = "name,ring,quadrant,isNew,moved,description"
+with open("specfile.csv", "w") as file:
+    file.write(header + "\n")
+    # add file content
+    for _ in range(lines):
+        # Create line as string
+        values = []
+        name = ""
+        for _ in range(random.randint(2,15)):
+            name += random.choice(string.ascii_letters)
+        values.append(name)
+        values.append(random.choice(["Hold", "Assess", "Trial", "Adopt"]))
+        values.append(random.choice(["data management", "datastore", "infrastructure", "language"]))
+        values.append(random.choice(["true", "false"]))
+        values.append(str(random.randint(-3,3)))
+        desc = ""
+        for _ in range(random.randint(20,150)):
+            desc += random.choice(string.ascii_letters)
+        values.append(desc)
+        file.write(",".join(values) + "\n")

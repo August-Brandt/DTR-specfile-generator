303c7a762dffb8c19003468292fa98042a892b01
Theis <112340858+Slug-Boi@users.noreply.github.com>
2024-05-23 11:26:21 +0200

    Update SpecfileGenerator.py

--- SpecfileGenerator.py
+++ SpecfileGenerator.py
@@ -1,7 +1,13 @@
 import random
 import string
+import sys
 
-lines = 1000 # <- Set the number of lines here
+args = sys.argv
+lines = 150
+
+if len(args) > 1:
+    lines = int(args[1])
+
 header = "name,ring,quadrant,isNew,moved,description"
 with open("specfile.csv", "w") as file:
     file.write(header + "\n")
@@ -10,17 +16,18 @@ with open("specfile.csv", "w") as file:
         # Create line as string
         values = []
         name = ""
-        for _ in range(random.randint(2,15)):
+        for _ in range(random.randint(2, 15)):
             name += random.choice(string.ascii_letters)
         values.append(name)
         values.append(random.choice(["hold", "assess", "trial", "adopt"]))
-        values.append(random.choice(["techniques", "platforms", "tools", "languages & frameworks"]))
+        values.append(random.choice(
+            ["techniques", "platforms", "tools", "languages & frameworks"]))
         values.append(random.choice(["true", "false"]))
-        values.append(str(random.randint(-3,3)))
+        values.append(str(random.randint(-3, 3)))
         desc = ""
-        for _ in range(random.randint(20,150)):
+        for _ in range(random.randint(20, 150)):
             desc += random.choice(string.ascii_letters)
         values.append(desc)
-        
+
         # Write line to file
         file.write(",".join(values) + "\n")
